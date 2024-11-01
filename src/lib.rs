b25a8367e5915ebb9844ad1e9caae410c6ec03f6
Vasile Gorcinschi <vasigorc@gmail.com>
2024-11-01 17:32:35 -0400

    tests: Added integration test for parallel requests

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod server;
 mod website_handler;
 
 pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
+  // default_path works only for cargo commands (test, run, etc.)
   let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
   let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
   let server = Server::new("127.0.0.1:8080".to_string());
